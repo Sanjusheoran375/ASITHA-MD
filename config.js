8dc0a89a0028a85d7f43e2e310e39fb6fa314908
Sanjusheoran375 <sanjusheoran375@gmail.com>
2024-10-18 05:44:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=Om50EQxL#Hb7Qvk2b1Hfdrq2HbqADwDW7oWexJTad3tyHn-ZZydA",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=26BSHDhT#Arl1Gzuf-NEEtKMSPRS4q9Yt2tPTv7JhG6W3TPV2z9g",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
